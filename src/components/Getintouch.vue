e3b37b8fdc58994c3533bcba79c70b4a5171320e
kimhayeon <kmhayeon@naver.com>
2019-12-23 14:54:24 +0900

    email url change

--- src/components/Getintouch.vue
+++ src/components/Getintouch.vue
@@ -109,7 +109,7 @@
                         console.error(xhr.responseText);
                     }
                 };
-                xhr.open('POST', 'http://206.189.151.251:3000');
+                xhr.open('POST', 'https://www.hacoding.com/email');
                 xhr.send(formData); // 폼 데이터 객체 전송
                 /*var xhr = new XMLHttpRequest();
 
